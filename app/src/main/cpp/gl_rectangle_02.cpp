f71bd3dbb368c8d4b29c3ff3ab0e5fc0406de74a
王阳 <wyroman@163.com>
2018-07-30 15:33:19 +0800

    优化代码

--- app/src/main/cpp/gl_rectangle_02.cpp
+++ app/src/main/cpp/gl_rectangle_02.cpp
@@ -27,19 +27,20 @@ namespace gl_rectangle_02 {
 
 #include "gl_rectangle_02.h"
 #include "util.cpp"
-using namespace util;
+	
+	using namespace util;
 	
 	const GLfloat vertices[] = {
 			0.5f, 0.5f, 0.0f,   // 右上角
 			0.5f, -0.5f, 0.0f,  // 右下角
 			-0.5f, -0.5f, 0.0f, // 左下角
 			-0.5f, 0.5f, 0.0f,   // 左上角
-			0.0f,1.0f,0.0f  //中间上
+			0.0f, 1.0f, 0.0f  //中间上
 	};
 	unsigned int indices[] = { // 注意索引从0开始!
 			0, 1, 3, // 第一个三角形
 			1, 2, 3,  // 第二个三角形
-			0,3,4,
+			0, 3, 4,
 	};
 	auto gVertexShader =
 			"attribute vec4 vPosition;\n"
@@ -57,6 +58,7 @@ using namespace util;
 	GLuint gvPositionHandle;
 	GLuint vertexbuffer;
 	unsigned int EBO;
+	
 	bool setupGraphics (int w, int h) {
 		gProgram = createProgram(gVertexShader, gFragmentShader);
 		if (!gProgram) {
@@ -64,19 +66,19 @@ using namespace util;
 			return false;
 		}
 		gvPositionHandle = glGetAttribLocation(gProgram, "vPosition");//属性绑定到 vPostion 关键字上
-		checkGlError("glGetAttribLocation");//创建
 		glViewport(0, 0, w, h);//指定绘图区域，（坐标x ,坐标y,坐标x+width,坐标y+height） 后两个参数不是坐标，而是用宽高来间接指定坐标
-		checkGlError("glViewport");
 		
-		
+		//创建顶点数组缓冲对象
 		glGenBuffers(1, &vertexbuffer);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		
 		//创建索引缓冲对象
-	
 		glGenBuffers(1, &EBO);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+		
+		
 		return true;
 	}
 	
@@ -92,32 +94,27 @@ using namespace util;
 		if (grey > 1.0f) {
 			grey = 0.0f;
 		}
-		glClearColor(grey, grey, grey, 1.0f);//设置屏幕背景色
-		checkGlError("glClearColor");
+		glEnableVertexAttribArray(gvPositionHandle);  //启用顶点位置数据
+//		glClearColor(grey, grey, grey, 1.0f);//设置屏幕背景色
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);//设置屏幕背景色
 		glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT); //清除深度缓冲与颜色缓冲
-		checkGlError("glClear");
 		
 		glUseProgram(gProgram);  //根据着色程序id 指定要使用的着色器
-		checkGlError("glUseProgram");
-//		glEnableVertexAttribArray(0);
-		glEnableVertexAttribArray(gvPositionHandle);//  //启用顶点位置数据
+		
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 		glVertexAttribPointer(
-				0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-				3,                  // size
-				GL_FLOAT,           // type
-				GL_FALSE,           // normalized?
-				0,                  // stride
-				(void*)0            // array buffer offset
+				0,                 // 顶点属性，即前面设置的gvPositionHandle                   
+				3,                 // 顶点属性的大小，这里为ve3 ,即size=3                       
+				GL_FLOAT,          // 顶点数据的数据类型                                     
+				GL_FALSE,          // 数据是否需要标准化 会被映射成0~1之间  有符号为-1到1                
+				0,                 // 连续的顶点属性之间的间隔                                  
+				(void *) 0           // 位置数据在缓冲中的偏移量                                
 		);
-		
-		// Draw the triangle !
-//        glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle 根据数据画三角形
 //		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);//线框模式
 //         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);//填充模式
 		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);//根据索引绘制
-		glDisableVertexAttribArray(0);
-	
+		glDisableVertexAttribArray(0);     //关闭顶点位置数据
+		
 	}
 	
 	JNIEXPORT void JNICALL
